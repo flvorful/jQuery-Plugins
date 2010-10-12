0e4c7d8dd2958d5152b758304cc5e1eae56de499
Peter Galiba <poetro@poetro.hu>
2010-10-12 22:29:29 +0200

    Fix attribute encoding issues. Cleaned up code to be JSLint confomr. Added and fixed tests.

--- easy_html/jquery.easy_html_tags.js
+++ easy_html/jquery.easy_html_tags.js
@@ -1,41 +1,76 @@
-(function($){
-	tag_helper = {
-		
-		tag_options: function(opts) {
-			if (!$.isEmptyObject(opts)) {
-				attr = []
-				$.each(opts, function(k, v) {
-					attr.push(k + "=\"" + v + "\"") 
-				})
-				return " " + attr.join(" ")
-			};
-			return ""
-		},
-		content_tag: function(name, content, options) {
-			attrs = ""
-			if (!$.isEmptyObject(options)) {
-				attrs = tag_helper.tag_options(options)
-			}
-      return "<" + name + attrs + ">" + content + "</" + name + ">"
-		}
-	}
-	$.t = function() {
-		return $($.tag(arguments[0], arguments[1], arguments[2], arguments[3]))
-	}
-	$.tag = function() {
-		if (!$.isEmptyObject)
-			$.error( 'jQuery 1.4.2 is needed for $.tag' );
-		
-		if (arguments.length > 4 || arguments.length < 1) {
-			return ""
-		};
-		var tag_type = arguments[0]
-		var content = typeof(arguments[1]) == "string" ? arguments[1] : ""
-		var opts = typeof(arguments[2]) == "object" ? arguments[2] : (typeof(arguments[1]) == "object" ? arguments[1] : {})
-		var block = $.isFunction(arguments[3]) ? arguments[3] : ($.isFunction(arguments[2]) ? arguments[2] :  ($.isFunction(arguments[1]) ? arguments[1] : undefined))
-		if (block != undefined) {
-			content += block.call()
-		};
-		return tag_helper.content_tag(tag_type, content, opts)
-	};
-})(jQuery);
+(function ($) {
+  var tag_helper = {
+    tag_options: function (opts) {
+      var attr = [],
+          output = '',
+          self = this;
+
+      if (!$.isEmptyObject(opts)) {
+        $.each(opts, function (k, v) {
+          var names = k.split(/(\s|\u00A0)+/),
+              i = names.length,
+              name;
+          if (i > 1) {
+            for (; i > 1; i -= 1) {
+              name = $.trim(names.shift());
+              if (name) {
+                attr.push(name);
+              }
+            }
+          }
+          attr.push(names[0] + '="' +
+            self.checkPlain(v) +
+            '"');
+        });
+        output = ' ' + attr.join(' ');
+      }
+      return output;
+    },
+
+    content_tag: function (name, content, options) {
+      var attrs = !$.isEmptyObject(options) ?
+            this.tag_options(options) :
+            "";
+      return "<" + name + attrs + ">" + content + "</" + name + ">";
+    },
+
+    checkPlain : function (text) {
+      return text.replace(/["&]/g, function (match) {
+        return match === '"' ? '&quot;' : '&amp;';
+      });
+    }
+  };
+
+  $.extend($, {
+    t: function () {
+      return $($.tag.apply($, arguments));
+    },
+
+    tag: function (tag_type, content, options, block) {
+      if (!$.isEmptyObject) {
+        throw new Error('jQuery 1.4 is needed for $.tag');
+      }
+
+      if (!$.isFunction(block)) {
+        block = $.isFunction(arguments[2]) ?
+          arguments[2] :
+          ($.isFunction(arguments[1]) ? arguments[1] : $.noop());
+      }
+      if (!$.isPlainObject(options)) {
+        options = $.isPlainObject(arguments[1]) ? arguments[1] : {};
+      }
+      if (typeof(content) !== "string") {
+        content = '';
+      }
+
+      if (arguments.length > 4 || arguments.length < 1) {
+        return "";
+      }
+      if (block) {
+        content += block.call();
+      }
+
+      return tag_helper.content_tag(tag_type, content, options);
+    }
+  });
+}(jQuery));
